ec389fc940449b42b2bb2a2d20839c3e24086db3
Lokman Hossen <lokmanhossen230@gmail.com>
2024-09-02 02:06:13 +0600

    Add appBar And Icon

--- lib/widget/expenses.dart
+++ lib/widget/expenses.dart
@@ -1,4 +1,5 @@
 import 'package:expense_tracker_flutter/widget/expenses_list/expenses_list.dart';
+import 'package:expense_tracker_flutter/widget/new_expense.dart';
 import 'package:flutter/material.dart';
 
 import '../models/expense.dart';
@@ -25,9 +26,24 @@ class _ExpensesState extends State<Expenses> {
       date: DateTime.now(),
     ),
   ];
+
+  void _openAddExpenseOverlay() {
+    showModalBottomSheet(
+        context: context, builder: (ctx) => const NewExpense());
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: const Text('Flutter ExpensesTracker'),
+        actions: [
+          IconButton(
+            onPressed: _openAddExpenseOverlay,
+            icon: const Icon(Icons.add),
+          ),
+        ],
+      ),
       body: Column(
         children: [
           const Text('The Chart'),
